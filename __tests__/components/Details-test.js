3f8dd67fba2f94161fb16aa4e06354924e331e8b
Helen Martch <helen@mobilexlabs.com>
2017-08-30 12:26:20 -0500

    add basic initial form elements

--- __tests__/components/Details-test.js
+++ __tests__/components/Details-test.js
@@ -15,4 +15,33 @@ describe('<Details />', () => {
     expect(_details.find('div.details')).toHaveLength(1);
   });
 
+  it('renders a form tag"', () => {
+    expect(_details.find('form')).toHaveLength(1);
+  });
+
+  it('renders 1 label tag when the panel_topic is "intro"', () => {
+    const _details = shallow(<Details panel_topic="intro"/>);
+    expect(_details.find('label')).toHaveLength(1);
+  });
+
+  it('renders 3 label tags when the panel_topic is "movies"', () => {
+    const _details = shallow(<Details panel_topic="movies"/>);
+    expect(_details.find('label')).toHaveLength(3);
+  });
+
+  it('renders 2 label tags with class "choice" when the panel_topic is "movies"', () => {
+    const _details = shallow(<Details panel_topic="movies"/>);
+    expect(_details.find('label.choice')).toHaveLength(2);
+  });
+
+  it('renders 1 input tag when the panel_topic is "intro"', () => {
+    const _details = shallow(<Details panel_topic="intro"/>);
+    expect(_details.find('input')).toHaveLength(1);
+  });
+
+  it('renders 2 input tags when the panel_topic is "movies"', () => {
+    const _details = shallow(<Details panel_topic="movies"/>);
+    expect(_details.find('input')).toHaveLength(2);
+  });
+
 });
--- src/js/components/Details.react.js
+++ src/js/components/Details.react.js
@@ -4,21 +4,26 @@ class Details extends React.Component {
   render() {
     if (this.props.panel_topic === 'intro') {
       var panel_details = (
-        <div>
-
-        </div>
+        <label>What is your name?
+          <input type="text" id="name" />
+        </label>
       );
     }
     else if (this.props.panel_topic === 'movies') {
       var panel_details = (
-        <div>
-          
-        </div>
+        <label>Do you have a favorite movie?
+          <label className="choice">Yes <input type="radio" name="fav-movie" value="Yes" /></label>
+          <label className="choice">No <input type="radio" name="fav-movie" value="No" /></label>
+        </label>
       );
     }
 
     return (
-      <div className="details">{panel_details}</div>
+      <div className="details">
+        <form>
+          {panel_details}
+        </form>
+      </div>
     )
   }
 }
