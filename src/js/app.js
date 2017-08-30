1ef75a6bb64a5010c531778902997a2c67f0fc0b
Helen Martch <helen@mobilexlabs.com>
2017-08-30 11:53:17 -0500

    correct renamed component

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import AppParent from '../js/components/AppParent.react.js';
-import Splash from '../js/components/Splash.react.js';
+import Page from '../js/components/Page.react.js';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={AppParent}>
       {/* add the routes here */}
-      <IndexRoute component={Splash}/>
+      <IndexRoute component={Page}/>
     </Route>
   </Router>
 ), document.getElementById('root'));
